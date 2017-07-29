4538104a337371956114ed0c9aaba791234878fe
Pluginer_Enc <z0502281868@gmail.com>
2017-07-28 19:40:00 -0800

    Create Main.php

---
+++ src/slapparo/Main.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace: slapparoMc;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as Color;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
